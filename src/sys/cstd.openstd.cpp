d976e97a9b8bd306156320eedfe811180e59aadf
StumbDev <stumbledeveloper@hotmail.com>
2025-05-13 13:43:23 -0300

    add more shit

---
+++ src/sys/cstd.openstd.cpp
@@ -0,0 +1,19 @@
+#include "Lemon.h"
+
+Lemon::Mod::FileType(lib, cxx11);
+Lemon::Mod::IntLib(
+    this.name == "cstd";
+    this.lang == "cxx";
+    this.entry == LibInit();
+    this.isStd? == false;
+    );
+pub void CStandardLibrary() {
+  void LibInit() {
+    //
+  }
+}
+
+main() {
+  Lemon::Mod::Export(this.lib);
+  return Lib;
+}
